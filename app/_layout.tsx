1d04a4a9ce3f2a754ee3a8e7c3e6e1da7269ccd5
odusdegreat <tobiodubote@gmail.com>
2025-11-02 17:38:13 +0100

    fix: fixed some errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,29 @@
 import "expo-dev-client";
-import "react-native-reanimated"; // 🧠 MUST be first
+import "react-native-reanimated"; // 👈 must be FIRST import
 import "../global.css";
 
+import { NAV_THEME } from "@/theme";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { ThemeProvider as NavThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 
-import { NAV_THEME } from "@/theme";
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
+export { ErrorBoundary } from "expo-router";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme() ?? "light";
-  const isDarkColorScheme = colorScheme === "dark";
+  const isDark = colorScheme === "dark";
 
   return (
     <>
       <StatusBar
-        key={`root-status-bar-${isDarkColorScheme ? "light" : "dark"}`}
-        style={isDarkColorScheme ? "light" : "dark"}
+        key={`root-status-bar-${isDark ? "light" : "dark"}`}
+        style={isDark ? "light" : "dark"}
       />
 
-      {/* Wrap the app in ActionSheet and Theme Providers */}
       <ActionSheetProvider>
         <NavThemeProvider value={NAV_THEME[colorScheme]}>
-          {/* Root navigation stack */}
           <Stack screenOptions={{ headerShown: false }} />
         </NavThemeProvider>
       </ActionSheetProvider>
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    presets: ["babel-preset-expo"],
+    plugins: [
       "nativewind/babel",
+      "react-native-reanimated/plugin", // 👈 must always be last
     ],
-    plugins: ["react-native-worklets/plugin", "react-native-reanimated/plugin"],
   };
 };
